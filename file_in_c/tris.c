80dd555434f0a108fafdb286e71fcf669e313a68
Sebastiano Lanfranchi <sebalanfro@gmail.com>
2024-11-15 23:43:11 +0100

    UPD

--- file_in_c/tris.c
+++ file_in_c/tris.c
@@ -1,4 +1,7 @@
 #include<stdio.h>
+
+#define M 5
+
 void campo_da_gioco(){
 
     //qui ci andra il campo da gioco
@@ -12,7 +15,43 @@ int main(){
     menu();
     campo_da_gioco();
 
+    int cnt = 0;
+    char matrice[M][M];
+
     char let1 = 'O';
     char let2 = 'X';
+    /*
+    printf("Giocatore 1 che pedina scegli?\n");
+    printf("scegli la X o il O???");*/
     
+
+    
+    // Creazone  della matrice
+     for (int i = 0; i < M; i++) {
+        for (int j = 0; j < M; j++) {
+               if( j%2 == 0 && i%2 ==0){
+                   matrice[i][j] = ' ';
+               }
+            else{
+                if(j%2 == 1){
+                    matrice[i][j] = '|';
+                }
+                if(i%2 == 1){
+                    matrice[i][j] = '-';
+                }
+             }
+         }
+    }
+    
+    // Stampa della matrice
+    printf("  TRIS\n");
+    for (int i = 0; i < M; i++) {
+        for (int j = 0; j < M; j++) {
+            printf("%c ", matrice[i][j]);
+        }
+        printf("\n");
+    }
+    
+    
+     
 }
