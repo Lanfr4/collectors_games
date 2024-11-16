b68c48b3bf4baeff2c0d24a841af2da63065778e
Sebastiano Lanfranchi <128606572+Lanfr4@users.noreply.github.com>
2024-11-16 16:43:23 +0000

    upd

--- file_in_c/tris.c
+++ file_in_c/tris.c
@@ -1,49 +1,9 @@
-#include<stdio.h>
+#include <stdio.h>
 
 #define M 5
 
-void campo_da_gioco(){
-
-    //qui ci andra il campo da gioco
-}
-void menu(){
-
-    // menu del gioco
-}
-int main(){
-
-    menu();
-    campo_da_gioco();
-
-    int cnt = 0;
-    char matrice[M][M];
-
-    char let1 = 'O';
-    char let2 = 'X';
-    /*
-    printf("Giocatore 1 che pedina scegli?\n");
-    printf("scegli la X o il O???");*/
-    
-
-    
-    // Creazone  della matrice
-     for (int i = 0; i < M; i++) {
-        for (int j = 0; j < M; j++) {
-               if( j%2 == 0 && i%2 ==0){
-                   matrice[i][j] = ' ';
-               }
-            else{
-                if(j%2 == 1){
-                    matrice[i][j] = '|';
-                }
-                if(i%2 == 1){
-                    matrice[i][j] = '-';
-                }
-             }
-         }
-    }
-    
-    // Stampa della matrice
+void campo_da_gioco(char matrice[M][M]) {
+    // Qui ci sarà il campo di gioco che si aggiornerà ogni volta
     printf("  TRIS\n");
     for (int i = 0; i < M; i++) {
         for (int j = 0; j < M; j++) {
@@ -51,7 +11,49 @@ int main(){
         }
         printf("\n");
     }
+}
+
+void menu() {
     
+    int s;
     
-     
+    printf("Benvenuto al gioco del Tris!\n");
+    do{
+        printf("Inserisci 1 per iniziare : %d\n ", s);
+        scanf("%d", &s);
+    }while(s!=0);
+}
+
+int main() {
+    menu();
+
+    int cnt = 0;
+    char matrice[M][M];
+
+    // Creazione della matrice
+    for (int i = 0; i < M; i++) {
+        for (int j = 0; j < M; j++) {
+            if (j % 2 == 0 && i % 2 == 0) {
+                matrice[i][j] = ' ';
+            } else {
+                if (j % 2 == 1) {
+                    matrice[i][j] = '|';
+                }
+                if (i % 2 == 1) {
+                    matrice[i][j] = '-';
+                }
+            }
+        }
+    }
+
+    // Mostra il campo di gioco
+    campo_da_gioco(matrice);
+
+    // Esempio di inserimento di un simbolo
+    matrice[0][0] = 'X'; // Giocatore 1
+    campo_da_gioco(matrice);
+
+    // Qui puoi aggiungere il codice per gestire i turni dei giocatori
+
+    return 0;
 }
