8647997bfeb2943fb0a66fa2e9ed7a6662d531e7
Sebastiano Lanfranchi <128606572+Lanfr4@users.noreply.github.com>
2024-11-19 10:59:16 +0100

    Update README.d

--- Roba di Seba/README.d
+++ Roba di Seba/README.d
@@ -1,5 +1,5 @@
-CARTELLLA DEI GIOCHI
+#CARTELLLA DEI GIOCHI
 ------------------------------
-Qui ci sono le cartelli dei giochi divisi nei linguaggi in  cui le faccio:
-  - In c
-  - In HTML, CSS e JavaScript
+Qui ci sono le cartelli dei giochi divisi nei linguaggi con cui li faccio:
+- In c
+- In HTML, CSS e JavaScript
